5090083d79e7bd518ccb529591b5b7787086e004
Alen Siljak <alen.siljak@gmx.com>
2018-03-06 10:07:12 +0100

    adding sample proxy call

--- finance_quote/app.py
+++ finance_quote/app.py
@@ -10,4 +10,9 @@ class App:
     """ The main entry point to the F::Q library """
     def __init__(self):
         self.logger = logging.getLogger(__name__)
+        self.modules = ['alphavantage']
 
+    def alphavantage(self):
+        """ example using av as provider """
+        # TODO invoke av module for fetching price
+        pass
