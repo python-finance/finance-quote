094ea6e18eea6b508b67fc69c422778a2ec8b122
GFJ138 <sebastien.dementen@engie.com>
2018-03-10 22:22:24 +0100

    change package name to remove -python

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='finance-quote-python',  # Required
+    name='finance-quote',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
