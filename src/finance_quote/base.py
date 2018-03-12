93cfdd1f35a2732413ad404bc2d798f6c9219f56
Alen Siljak <alen.siljak@gmx.com>
2018-03-12 10:33:18 +0100

    adding the initial Quote model

--- src/finance_quote/base.py
+++ src/finance_quote/base.py
@@ -18,7 +18,19 @@ class Symbol:
 
 class Quote:
     """Class to represent a quote (price, ...)"""
-    pass
+    def __init__(self):
+        self.datetime: datetime = None
+        self.namespace: str = None
+        self.symbol: str = None
+        self.value: Decimal = Decimal(0)
+        self.currency: str = None
+
+    def __repr__(self):
+        symbol = f"{self.namespace}:{self.symbol}" if self.namespace else self.symbol
+        symbol = f"{symbol:<13}"
+
+        value = f"{self.value:>6}"
+        return f"<Quote ('{symbol}',date:{self.datetime},value:{value},currency:{self.currency})>"
 
 class Source:
     """Class to represent a source of symbols or quotes"""
